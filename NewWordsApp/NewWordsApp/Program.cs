86729c634930f8a3be8d7412ef547d50b5282860
Diana <dianaganushkevych@gmail.com>
2020-10-09 16:01:08 +0300

    Empty Console App project Created

---
+++ NewWordsApp/NewWordsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewWordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
