a762ee9af93b895e25a8958e485a001e55afe45d
Diana <dianaganushkevych@gmail.com>
2020-10-26 18:21:06 +0200

    Edited BLL layer

--- NewWords/BLL/Autorization.cs
+++ NewWords/BLL/Autorization.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BusinessLogicLayer
+namespace BLL
 {
     class Autorization
     {
@@ -27,4 +27,3 @@ namespace BusinessLogicLayer
         }
     }
 }
-
